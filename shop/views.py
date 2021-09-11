0b7147452f3241d2e08a2ddb3df1a96e7e207d07
Ainaz <grimoruu@yandex.ru>
2021-09-12 00:09:15 +0300

    new project

---
+++ shop/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
+
+# Create your views here.
+
+
+def home(request, category_slug=None):
+	category_page = None
+	products = None
+	if category_slug != None:
+		category_page = get_object_or_404(Category, slug = category_slug)
+		products = Product.objects.filter(category = category_page, available = True)
+	else:
+		products = Product.objects.all().filter(available = True)
+	return render(request, 'home.html', {'category': category_page, 'products': products})
+
+
+def product(request):
+    return render(request, 'product.html')
+
